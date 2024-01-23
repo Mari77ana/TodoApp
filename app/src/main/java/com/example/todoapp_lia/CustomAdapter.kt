5358b8918dfea7f695d3c3d83dc2576ab0bfdcb3
m.laic <m.laic@hotmail.com>
2024-01-23 16:10:40 +0100

    add fragment, activity_layout_xml,created CustomAdapter

---
+++ app/src/main/java/com/example/todoapp_lia/CustomAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.todoapp_lia
+
+class CustomAdapter {
+}
--- app/src/main/java/com/example/todoapp_lia/MainActivity.kt
+++ app/src/main/java/com/example/todoapp_lia/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.todoapp_lia.ui.theme.TodoApp_liaTheme
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
